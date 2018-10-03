ba0b0612a1592ac226574665b3fddd25cd1099f6
David Schuppa <d.schuppa@gmail.com>
2018-10-03 13:21:01 +0200

    added GrowPowerup class and new png for it

---
+++ src/com/codecool/snake/entities/powerups/GrowPowerup.java
@@ -0,0 +1,9 @@
+package com.codecool.snake.entities.powerups;
+
+import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.entities.Interactable;
+
+public class GrowPowerup extends GameEntity implements Interactable {
+
+
+}
