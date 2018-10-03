d2548ac8d82c123e572bb24595c3ba0bc9b572ae
David Schuppa <d.schuppa@gmail.com>
2018-10-03 10:40:21 +0200

    implemented Interactable methods in HealthPowerup class, added new png to resources, created new field in Globals for the powerup

--- src/com/codecool/snake/entities/powerups/HealthPowerup.java
+++ src/com/codecool/snake/entities/powerups/HealthPowerup.java
@@ -1,4 +1,34 @@
 package com.codecool.snake.entities.powerups;
 
-public class HealthPowerup {
+import com.codecool.snake.Globals;
+import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.scene.layout.Pane;
+
+import java.util.Random;
+
+/* powerup that increase snakes health by 10 points */
+public class HealthPowerup extends GameEntity implements Interactable {
+
+    public HealthPowerup(Pane pane) {
+        super(pane);
+        setImage(Globals.healthPowerup);
+        pane.getChildren().add(this);
+
+        Random rnd = new Random();
+        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+    }
+
+    @Override
+    public void apply(SnakeHead snakeHead){
+        snakeHead.changeHealth(10);
+        destroy();
+    }
+
+    @Override
+    public String getMessage(){
+        return "Got +10 health";
+    }
 }
