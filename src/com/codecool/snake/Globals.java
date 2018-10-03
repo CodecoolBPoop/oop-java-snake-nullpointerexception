be44ee151fb01bf19c69f7e2bc71570cfb3b5054
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-03 09:36:32 +0200

    declared running boolean variable

--- src/com/codecool/snake/Globals.java
+++ src/com/codecool/snake/Globals.java
@@ -26,6 +26,7 @@ public class Globals {
     public static List<GameEntity> newGameObjects; // Holds game objects crated in this frame.
     public static List<GameEntity> oldGameObjects; // Holds game objects that will be destroyed this frame.
     public static GameLoop gameLoop;
+    public static boolean running;
 
     static {
         gameObjects = new LinkedList<>();
